d01225d81001d1293990c76ad13219530e87e437
Rishabh0059 <rishabh1259@gmail.com>
2024-10-09 20:14:12 +0530

    updated resume link

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -54,7 +54,7 @@ const SocialLinks = () => {
        {Links.map(({id,child,href,style})=>(
        <li key={id} className={"flex justify-between items-center w-40 h-14 px-4 ml-[-100px]  hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" + "" + style}>
         <a href={href} className="flex justify-between items-center w-full text-white"
-        
+        target="_blank" rel="noopner noreferrer"
         >{child}</a>
                 </li>
        ))}
